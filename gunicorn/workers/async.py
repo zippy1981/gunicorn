1cdf240c848418d0d8513cb027faafb2aa4b622b
benoitc <bchesneau@gmail.com>
2010-12-25 12:50:52 +0100

    s/http/wsgi/g

--- gunicorn/workers/async.py
+++ gunicorn/workers/async.py
@@ -26,7 +26,7 @@ class AsyncWorker(base.Worker):
 
     def handle(self, client, addr):
         try:
-            parser = http.WSGIRequestParser(client)
+            parser = wsgi.WSGIRequestParser(client)
             try:
                 while True:
                     req = None
